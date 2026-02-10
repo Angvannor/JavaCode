fe6e8719fc6ad6630dde2a6c360b1a3ff2140def
Angvannor <3102853063@qq.com>
2026-02-10 17:52:55 +0800

    标识符学习

---
+++ Chapter02/Homework03.java
@@ -0,0 +1,13 @@
+public class Homework03{
+    public static void main(String[] args){
+        String book1 = "The lord of the rings";
+        String book2 = "The Hobbits";
+        System.out.println(book1 + '\t' + book2);
+        char c1 = '男';
+        char c2 = '女';
+        System.out.println(c1 + c2);
+        double price1 = 199.8;
+        double price2 = 99.8;
+        System.out.println(price1 + price2);
+    }
+}
---
+++ Chapter02/Homework04.java
@@ -0,0 +1,10 @@
+public class Homework04{
+    public static void main(String[] args){
+        String name = "Aragorn";
+        int age = 84;
+        double score = 80.9;
+        char gender = '男';
+        String hobby = "Ranger";
+        System.out.println("姓名\t年龄\t分数\t性别\t爱好\n" + name + '\t' + age + '\t' + score + '\t' + gender + '\t' + hobby);
+    }
+}
